21eb1bf0f9f1a3e029913163221b2f832970b441
Brian Morton <brian@mmm.hm>
2015-01-14 02:18:53 -0800

    First naive implementation of task execution where output is just sent to console, but task is executed with Docker, output is streamed, and container is cleaned up.

--- server/tasks_resource.go
+++ server/tasks_resource.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"bytes"
+	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 
@@ -22,6 +25,44 @@ type Task struct {
 }
 
 func (tr *TasksResource) Create(u *url.URL, h http.Header, req *TaskRequest) (int, http.Header, interface{}, error) {
+	serviceName := u.Query().Get("name")
+	taskName := fmt.Sprintf("%s-%s-task", serviceName, req.Task.Version)
+	imageName := fmt.Sprintf("%s/%s:%s", tr.DockerHubUsername, serviceName, req.Task.Version)
+
+	container, err := tr.Docker.CreateContainer(docker.CreateContainerOptions{
+		Name: taskName,
+		Config: &docker.Config{
+			Image:        imageName,
+			Cmd:          []string{req.Task.Command},
+			AttachStdout: true,
+			AttachStderr: true,
+		},
+	})
+	if err != nil {
+		return http.StatusInternalServerError, nil, nil, err
+	}
+
+	log.Printf("%#v", container)
+	tr.Docker.StartContainer(container.ID, &docker.HostConfig{})
+
+	var buf bytes.Buffer
+	err = tr.Docker.AttachToContainer(docker.AttachToContainerOptions{
+		Container:    container.ID,
+		OutputStream: &buf,
+		Logs:         true,
+		Stdout:       true,
+		Stderr:       true,
+		Stream:       true,
+	})
+	if err != nil {
+		return http.StatusInternalServerError, nil, nil, err
+	}
+	log.Println(buf.String())
+
+	err = tr.Docker.RemoveContainer(docker.RemoveContainerOptions{ID: container.ID})
+	if err != nil {
+		return http.StatusInternalServerError, nil, nil, err
+	}
 
 	return http.StatusCreated, nil, nil, nil
 }
