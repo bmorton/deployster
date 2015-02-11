8bfb9e241148f907090a914303cc76334bd86220
Brian Morton <brian@mmm.hm>
2015-02-11 18:52:11 -0500

    Rename DockerHubUsername to ImagePrefix so that it can be used for either the public docker hub registry username or a private registry URL.  Add documentation to various structs, methods, and resources.

--- server/deployster_service.go
+++ server/deployster_service.go
@@ -8,24 +8,33 @@ import (
 	"github.com/rcrowley/go-tigertonic"
 )
 
+// DeploysterService is the HTTP server that ties together all the resources,
+// configures routing and dependencies, and authenticates requests.  The server
+// will listen for requests in the address:port that is passed as the listen
+// string.  The provided username/password will be used for HTTP basic auth for
+// all requests.  An image prefix can be either a private registry address:port
+// or a username on the public registry (basically something that'll be appended
+// to the service name, e.g. mmmmhm/servicename or my.registry:5000/servicename).
 type DeploysterService struct {
-	AppVersion        string
-	Listen            string
-	Username          string
-	Password          string
-	DockerHubUsername string
-	RootMux           *tigertonic.TrieServeMux
-	Mux               *tigertonic.TrieServeMux
-	Server            *tigertonic.Server
+	AppVersion  string
+	Listen      string
+	Username    string
+	Password    string
+	ImagePrefix string
+	RootMux     *tigertonic.TrieServeMux
+	Mux         *tigertonic.TrieServeMux
+	Server      *tigertonic.Server
 }
 
-func NewDeploysterService(listen string, version string, username string, password string, dockerHubUsername string) *DeploysterService {
+// NewDeploysterService returns a configured DeploysterService, ready to listen
+// for HTTP requests via the provided listen string.
+func NewDeploysterService(listen string, version string, username string, password string, imagePrefix string) *DeploysterService {
 	service := DeploysterService{
-		Listen:            listen,
-		AppVersion:        version,
-		Username:          username,
-		Password:          password,
-		DockerHubUsername: dockerHubUsername,
+		Listen:      listen,
+		AppVersion:  version,
+		Username:    username,
+		Password:    password,
+		ImagePrefix: imagePrefix,
 	}
 	service.RootMux = tigertonic.NewTrieServeMux()
 	service.Mux = tigertonic.NewTrieServeMux()
@@ -36,12 +45,14 @@ func NewDeploysterService(listen string, version string, username string, passwo
 	return &service
 }
 
+// ConfigureRoutes sets up resources and their dependencies so that we can
+// configure all the HTTP routes that will be supported by the server.
 func (ds *DeploysterService) ConfigureRoutes() {
 	fleetClient := fleet.NewClient("/var/run/fleet.sock")
 	dockerClient, _ := docker.NewClient("unix:///var/run/docker.sock")
-	deploys := DeploysResource{&fleetClient, ds.DockerHubUsername}
+	deploys := DeploysResource{&fleetClient, ds.ImagePrefix}
 	units := UnitsResource{&fleetClient}
-	tasks := TasksResource{dockerClient, ds.DockerHubUsername}
+	tasks := TasksResource{dockerClient, ds.ImagePrefix}
 
 	ds.Mux.Handle("GET", "/version", ds.authenticated(tigertonic.Version(ds.AppVersion)))
 	ds.Mux.Handle("POST", "/services/{name}/deploys", ds.authenticated(tigertonic.Marshaled(deploys.Create)))
@@ -50,18 +61,22 @@ func (ds *DeploysterService) ConfigureRoutes() {
 	ds.Mux.Handle("POST", "/services/{name}/tasks", ds.authenticated(http.HandlerFunc(tasks.Create)))
 }
 
+// ListenAndServe starts the HTTP server.
 func (ds *DeploysterService) ListenAndServe() error {
 	return ds.Server.ListenAndServe()
 }
 
+// ListenAndServe starts the HTTPS server with the given certificate and key.
 func (ds *DeploysterService) ListenAndServeTLS(certPath string, keyPath string) error {
 	return ds.Server.ListenAndServeTLS(certPath, keyPath)
 }
 
+// Close gracefully stops listening for new requests
 func (ds *DeploysterService) Close() error {
 	return ds.Server.Close()
 }
 
+// authenticated wraps an HTTP handler with HTTP basic authentication
 func (ds *DeploysterService) authenticated(h http.Handler) tigertonic.FirstHandler {
 	return tigertonic.HTTPBasicAuth(
 		map[string]string{ds.Username: ds.Password},
--- server/templates.go
+++ server/templates.go
@@ -9,9 +9,9 @@ After=docker.service
 EnvironmentFile=/etc/environment
 User=core
 TimeoutStartSec=0
-ExecStartPre=/usr/bin/docker pull {{.DockerHubUsername}}/{{.Name}}:{{.Version}}
+ExecStartPre=/usr/bin/docker pull {{.ImagePrefix}}/{{.Name}}:{{.Version}}
 ExecStartPre=-/usr/bin/docker rm -f {{.Name}}-{{.Version}}-%i
-ExecStart=/usr/bin/docker run --name {{.Name}}-{{.Version}}-%i -p 3000 {{.DockerHubUsername}}/{{.Name}}:{{.Version}}
+ExecStart=/usr/bin/docker run --name {{.Name}}-{{.Version}}-%i -p 3000 {{.ImagePrefix}}/{{.Name}}:{{.Version}}
 ExecStartPost=/bin/sh -c "sleep 15; /usr/bin/etcdctl set /vulcand/upstreams/{{.Name}}/endpoints/{{.Name}}-{{.Version}}-%i http://$COREOS_PRIVATE_IPV4:$(echo $(/usr/bin/docker port {{.Name}}-{{.Version}}-%i 3000) | cut -d ':' -f 2)"
 ExecStop=/bin/sh -c "/usr/bin/etcdctl rm '/vulcand/upstreams/{{.Name}}/endpoints/{{.Name}}-{{.Version}}-%i' ; /usr/bin/docker rm -f {{.Name}}-{{.Version}}-%i"
 `
