48dc824e5796d205c419235eeee322773fab5963
Brian Morton <brian@mmm.hm>
2015-02-17 22:16:33 -0800

    Add documentation for versioned unit in the server package.

--- server/versioned_unit.go
+++ server/versioned_unit.go
@@ -4,6 +4,10 @@ import (
 	"github.com/bmorton/deployster/fleet"
 )
 
+// VersionedUnit is our representation of a Fleet unit.  Largely, the difference
+// is that Fleet units don't care about versioning, so this lets us bridge the
+// gap by exploding data that is encoded in the Fleet unit name into the proper
+// fields appropriate for deployster.
 type VersionedUnit struct {
 	Service      string `json:"service"`
 	Instance     string `json:"instance"`
@@ -13,6 +17,11 @@ type VersionedUnit struct {
 	MachineID    string `json:"machine_id"`
 }
 
+// FindServiceUnits parses an array of units returned from fleet and looks for
+// only the units that match the given service name, which is a subset of the
+// Fleet unit name.  It collects all those units and returns an array of
+// VersionedUnit structs that have had their additional deployster-specific
+// fields populated from the Fleet unit name.
 func FindServiceUnits(serviceName string, units []fleet.Unit) []VersionedUnit {
 	versionedUnits := []VersionedUnit{}
 
@@ -34,6 +43,10 @@ func FindServiceUnits(serviceName string, units []fleet.Unit) []VersionedUnit {
 	return versionedUnits
 }
 
+// FindServiceUnits parses an array of units returned from fleet and collects
+// all the versions present for the given service.  This doesn't currently look
+// at the state of the units, it simply looks for matching service names and
+// returns an array of all the unique versions found.
 func FindServiceVersions(serviceName string, units []fleet.Unit) []string {
 	uniqueVersions := make(map[string]bool)
 
