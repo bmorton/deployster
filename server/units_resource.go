5ed6c7d15b285dc3006b9db8bb62fb8fb73ac33c
Brian Morton <brian@mmm.hm>
2015-02-17 22:42:27 -0800

    Add documentation for UnitsResource in the server package.

--- server/units_resource.go
+++ server/units_resource.go
@@ -8,14 +8,24 @@ import (
 	"github.com/bmorton/deployster/fleet"
 )
 
+// UnitsResource is the HTTP resource responsible for getting basic information
+// on all units that exist for a given service.
 type UnitsResource struct {
 	Fleet fleet.Client
 }
 
+// UnitsResponse is the wrapper struct for the JSON payload returned by the
+// Index action.
 type UnitsResponse struct {
 	Units []VersionedUnit `json:"units"`
 }
 
+// Index is the GET endpoint for listing all units that exist for a given
+// service.
+//
+// This function assumes that it is nested inside `/services/{name}`
+// and that Tigertonic is extracting the service name and providing it via query
+// params.
 func (ur *UnitsResource) Index(u *url.URL, h http.Header, req interface{}) (int, http.Header, *UnitsResponse, error) {
 	statusCode := http.StatusOK
 	response := &UnitsResponse{}
