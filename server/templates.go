e01acb8c679497173538e56e6fc286bfed1d1c2f
Brian Morton <brian@mmm.hm>
2015-03-08 20:38:05 -0700

    Lengthen sleep again for now.  We need a better long term solution for this.

--- server/templates.go
+++ server/templates.go
@@ -20,6 +20,6 @@ TimeoutStartSec=0
 ExecStartPre=/usr/bin/docker pull {{.ImagePrefix}}/{{.Name}}:{{.Version}}
 ExecStartPre=-/usr/bin/docker rm -f {{.Name}}-{{.Version}}-{{.Timestamp}}-%i
 ExecStart=/usr/bin/docker run --name {{.Name}}-{{.Version}}-{{.Timestamp}}-%i -p 3000 {{.ImagePrefix}}/{{.Name}}:{{.Version}}
-ExecStartPost=/bin/sh -c "sleep 3; /usr/bin/etcdctl set /vulcand/upstreams/{{.Name}}/endpoints/{{.Name}}-{{.Version}}-{{.Timestamp}}-%i http://$COREOS_PRIVATE_IPV4:$(echo $(/usr/bin/docker port {{.Name}}-{{.Version}}-{{.Timestamp}}-%i 3000) | cut -d ':' -f 2)"
+ExecStartPost=/bin/sh -c "sleep 10; /usr/bin/etcdctl set /vulcand/upstreams/{{.Name}}/endpoints/{{.Name}}-{{.Version}}-{{.Timestamp}}-%i http://$COREOS_PRIVATE_IPV4:$(echo $(/usr/bin/docker port {{.Name}}-{{.Version}}-{{.Timestamp}}-%i 3000) | cut -d ':' -f 2)"
 ExecStop=/bin/sh -c "/usr/bin/etcdctl rm '/vulcand/upstreams/{{.Name}}/endpoints/{{.Name}}-{{.Version}}-{{.Timestamp}}-%i' ; /usr/bin/docker rm -f {{.Name}}-{{.Version}}-{{.Timestamp}}-%i"
 `
